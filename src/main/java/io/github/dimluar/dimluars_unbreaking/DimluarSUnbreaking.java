005e74ffef8b0bea1ca1c4fc524733540b0a7ac7
Dimluar <148585902+Dimluar@users.noreply.github.com>
2026-08-11 11:42:20 +0200

    Initial Commit

---
+++ src/main/java/io/github/dimluar/dimluars_unbreaking/DimluarSUnbreaking.java
@@ -0,0 +1,22 @@
+package io.github.dimluar.dimluars_unbreaking;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DimluarSUnbreaking implements ModInitializer {
+	public static final String MOD_ID = "dimluars-unbreaking";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
---
+++ src/main/java/io/github/dimluar/dimluars_unbreaking/DimluarSUnbreakingClient.java
@@ -0,0 +1,10 @@
+package io.github.dimluar.dimluars_unbreaking;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DimluarSUnbreakingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
