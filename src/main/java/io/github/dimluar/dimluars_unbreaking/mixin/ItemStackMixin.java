620d9b8c32a4cfe63a64c985f2d0220cb2b0ddf8
Dimluar <148585902+Dimluar@users.noreply.github.com>
2026-08-11 19:58:13 +0200

    Make items unbreakable

---
+++ src/main/java/io/github/dimluar/dimluars_unbreaking/mixin/ItemStackMixin.java
@@ -0,0 +1,23 @@
+package io.github.dimluar.dimluars_unbreaking.mixin;
+
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.ItemStack;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+	@Shadow
+	public abstract <T> T set(DataComponentType<T> type, @Nullable T value);
+
+	@Inject(method = "setDamageValue", at = @At("TAIL"), cancellable = true)
+	public void setDamageValue(final int value, final CallbackInfo ci) {
+		this.set(DataComponents.DAMAGE, 0);
+		ci.cancel();
+	}
+}
